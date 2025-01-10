2992e90aefe5c6c428f5826c7832ea9e6b4fce98
AayushParashar28 <ayushparashar6203@gmail.com>
2025-01-10 23:16:46 +0530

    JavaScript

---
+++ async.js
@@ -0,0 +1,17 @@
+// Web APIs 
+//SetTimeout
+//Dom APIs -  Document Object Model
+// Local Storage
+// console
+// location and many more
+
+// Whar Comes under Microtask queue
+// 1. Promise
+// 2. DOM Mutation
+// 3. DOM Event
+// 4. DOM MutationObserver
+// 5. setTimeout with 0ms delay
+// 6. requestAnimationFrame
+// 7. requestIdleCallback
+// 8. requestAnimationFrame with 0ms delay
+
